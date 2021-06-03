97472ed62ee4f6290a109ab50e7faea2d5e35e1c
sandrasmrekar <sandrasmrekar98@gmail.com>
2021-06-03 15:14:10 +0200

    links clean up and hovver effect

--- src/components/LinkIcons/index.js
+++ src/components/LinkIcons/index.js
@@ -1,36 +1,14 @@
 import { ReactComponent as EmailSvg } from "../../assets/svg/email.svg";
 import { ReactComponent as GithubSvg } from "../../assets/svg/github.svg";
 import { ReactComponent as LinkedInSvg } from "../../assets/svg/linkedIn.svg";
-
-// TODO: Clean up, add links, hoover effect so they jump up
+import styles from "./index.module.css";
 
 export default function LinkIcons() {
   return (
-    <div style={{ position: "fixed", right: "40px", bottom: "40px" }}>
-      <EmailSvg
-        style={{
-          width: "30",
-          height: "30",
-          margin: "0 10px 0 10px",
-          cursor: "pointer",
-        }}
-      />
-      <GithubSvg
-        style={{
-          width: "30",
-          height: "30",
-          margin: "0 10px 0 10px",
-          cursor: "pointer",
-        }}
-      />
-      <LinkedInSvg
-        style={{
-          width: "30",
-          height: "30",
-          margin: "0 10px 0 10px",
-          cursor: "pointer",
-        }}
-      />
+    <div className={styles.container}>
+      <EmailSvg className={styles.icon} />
+      <GithubSvg className={styles.icon} />
+      <LinkedInSvg className={styles.icon} />
     </div>
   );
 }
