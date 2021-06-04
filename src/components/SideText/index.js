af940f3f1f5960c5bd30024645ba13fdffc5ccef
sandrasmrekar <sandrasmrekar98@gmail.com>
2021-06-04 16:18:58 +0200

    added icon row

--- src/components/SideText/index.js
+++ src/components/SideText/index.js
@@ -1,8 +1,8 @@
 import styles from "./index.module.css";
 
-export default function SideText({ label }) {
+export default function SideText({ label, style }) {
   return (
-    <div className={styles.container}>
+    <div className={styles.container} style={{ ...style }}>
       <div className={styles.bar} />
       <p className={styles.text}>{label}</p>
     </div>
--- src/screens/AboutMeScreen.js
+++ src/screens/AboutMeScreen.js
@@ -1,11 +1,15 @@
 import SideText from "../components/SideText";
 import styles from "./AboutMeScreen.module.css";
+import { ReactComponent as ReactIcon } from "../assets/svg/react.svg";
+import { ReactComponent as JavaIcon } from "../assets/svg/java.svg";
+import { ReactComponent as CsharpIcon } from "../assets/svg/csharp.svg";
+import { ReactComponent as JavascriptIcon } from "../assets/svg/javascript.svg";
 
 export default function AboutMeScreen() {
   return (
     <div className={styles.container}>
       <div className={styles.box}>
-        <SideText label="about me" />
+        <SideText label="about me" style={{ left: "-50px", top: "120px" }} />
         <div className={styles.textContainer}>
           <h3 className={styles.title}>Looking for an awesome collegue</h3>
           <p className={styles.p}>
@@ -13,6 +17,19 @@ export default function AboutMeScreen() {
             Just graduated from Malmö Sweden, majoring in Computer science and
             application development. Specializing in front end development{" "}
           </p>
+        </div>
+      </div>
+      <div className={styles.skillsContainer}>
+        <SideText label="skills" style={{ left: "-90px", top: "40px" }} />
+        <h3>Skills</h3>
+        <div className={styles.iconContainer}>
+          <ReactIcon className={styles.icon} />
+          <div className={styles.bar} />
+          <JavascriptIcon className={styles.icon} />
+          <div className={styles.bar} />
+          <JavaIcon className={styles.icon} />
+          <div className={styles.bar} />
+          <CsharpIcon className={styles.icon} />
         </div>
       </div>
     </div>
