ef2b54c5ddff3f8d66a6dfc66bef6e3266ff5dc8
sandrasmrekar <sandrasmrekar98@gmail.com>
2021-06-04 15:43:57 +0200

    added about me text

--- src/screens/AboutMeScreen.js
+++ src/screens/AboutMeScreen.js
@@ -1,5 +1,20 @@
+import SideText from "../components/SideText";
 import styles from "./AboutMeScreen.module.css";
 
 export default function AboutMeScreen() {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.box}>
+        <SideText label="about me" />
+        <div className={styles.textContainer}>
+          <h3 className={styles.title}>Looking for an awesome collegue</h3>
+          <p className={styles.p}>
+            Software engineer with a lot of passion for building cool stuff.
+            Just graduated from Malmö Sweden, majoring in Computer science and
+            application development. Specializing in front end development{" "}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
