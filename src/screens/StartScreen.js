6362b0107469a05c41214415c6241ed3fe7fc0d9
sandrasmrekar <sandrasmrekar98@gmail.com>
2021-06-15 16:32:08 +0200

    added start screen background animation

--- src/screens/StartScreen.js
+++ src/screens/StartScreen.js
@@ -2,12 +2,38 @@
  * This is the landing screen.
  */
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import LinkIcons from "../components/LinkIcons";
 import { START_SECTION } from "../constants/sections";
 import styles from "./StartScreen.module.css";
 
 export default function StartScreen() {
+  const [y, setY] = useState(window.scrollY);
+  const backgroundElement = document.getElementById("background");
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
+
+  const handleScroll = (e) => {
+    if (y > window.scrollY && window.scrollY < 40) {
+      // Scrolling upp
+      backgroundElement.style.right = "0px";
+      backgroundElement.style.left = "0px";
+      backgroundElement.style.top = "0px";
+      backgroundElement.style.bottom = "0px";
+    } else if (y < window.scrollY && window.scrollY > 20) {
+      // Scrolling down
+      backgroundElement.style.right = "50px";
+      backgroundElement.style.left = "50px";
+      backgroundElement.style.top = "50px";
+      backgroundElement.style.bottom = "50px";
+    }
+    setY(window.scrollY);
+  };
   return (
     <div id={START_SECTION} className={styles.container}>
       <div id="background" className={styles.background} />
