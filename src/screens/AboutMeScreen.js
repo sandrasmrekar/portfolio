99b2e67a5159fb47bfcbe2dc6a4f5085f2860727
sandrasmrekar <sandrasmrekar98@gmail.com>
2021-06-16 20:31:58 +0200

    added first draft of text

--- src/screens/AboutMeScreen.js
+++ src/screens/AboutMeScreen.js
@@ -28,6 +28,20 @@ function LogoItem({ children, title, text, noBar }) {
 
 // TODO dele upp i fler functioner eller komponenter
 export default function AboutMeScreen() {
+  const javaText =
+    "Java was the starting point of my software developer career. I utilized java for many of my school projects, from building smaller programs to larger desktop applications. Used (critital thinking skills) and object oriented programming aspects to build awesome applications...read more about dss ";
+  const javascriptText =
+    "Java Script along with HTML and CSS, opened my eyes for web development. I integrated various open source API's to build scool projects... With CSS (skills) I applied styles.. responsive user interface..bootstrap";
+  const reactText =
+    "It started with a school project, where React became a favourable java script framework for building highly responsive web based user interfaces by implementing React concepts.";
+  const reactNativeText =
+    "After exploring web development, mobile development was next on the list I wanted to learn. I got an amazing opportunity to learn the fundamentals of React Native at an internship";
+  const databaseText =
+    "Though i mostly specialités in frontens development, learing and understa dyning back end development has been an importstopp part for building imperativet and complete projects. Ser my e Vimmerby project to learn more how i user Merkel stack to build the website .";
+  const githubText =
+    "Github has been the main source version management tool that i have used for my projects. Link to my hub.";
+  const agileDevText =
+    "Agile development is an iterative approach to project management and software development.. that I implemented in the majority of my projects..scrumm successfull..";
   return (
     <div id={ABOUT_ME_SECTION} className={styles.container}>
       <div className={styles.box}>
@@ -46,17 +60,17 @@ export default function AboutMeScreen() {
           <SideText label="skills" style={{ left: "-90px", top: "40px" }} />
           <h3>Skills</h3>
           <div className={styles.iconContainer}>
-            <LogoItem title="React" text="hdhdhdhdhdhdhdh">
-              <ReactIcon className={styles.icon} />
-            </LogoItem>
-            <LogoItem title="Java Script" text="hdhdhdhdhdhdhdh">
-              <JavascriptIcon className={styles.icon} />
-            </LogoItem>
-            <LogoItem title="Java" text="hdhdhdhdhdhdhdh">
+            <LogoItem title="Java" text={javaText}>
               <JavaIcon className={styles.icon} />
             </LogoItem>
-            <LogoItem title="C Sharp" text="hdhdhdhdhdhdhdh" noBar>
-              <CsharpIcon className={styles.icon} />
+            <LogoItem title="Java Script" text={javascriptText}>
+              <JavascriptIcon className={styles.icon} />
+            </LogoItem>
+            <LogoItem title="React" text={reactText}>
+              <ReactIcon className={styles.icon} />
+            </LogoItem>
+            <LogoItem title="React Native" text={reactNativeText}>
+              <ReactIcon className={styles.icon} />
             </LogoItem>
           </div>
         </div>
@@ -65,13 +79,13 @@ export default function AboutMeScreen() {
           <SideText label="skills" style={{ left: "-90px", top: "40px" }} />
           <h3>Additional knowledge</h3>
           <div className={styles.iconContainer}>
-            <LogoItem title="Agile development" text="hdhdhdhdhdhdhdh">
+            <LogoItem title="Agile development" text={agileDevText}>
               <AgileIcon className={styles.icon} />
             </LogoItem>
-            <LogoItem title="Git" text="hdhdhdhdhdhdhdh">
+            <LogoItem title="Git" text={githubText}>
               <GitIcon className={styles.icon} />
             </LogoItem>
-            <LogoItem title="Database" text="hdhdhdhdhdhdhdh" noBar>
+            <LogoItem title="Database" text={databaseText} noBar>
               <DatabaseIcon className={styles.icon} />
             </LogoItem>
           </div>
