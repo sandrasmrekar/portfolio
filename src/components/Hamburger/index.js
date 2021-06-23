43e180f18108f7401bbd96ae5e8ef41976cdc231
sandrasmrekar <sandrasmrekar98@gmail.com>
2021-06-23 09:44:27 +0200

    hamburger transition

--- src/components/Hamburger/index.js
+++ src/components/Hamburger/index.js
@@ -1,6 +1,29 @@
+import { useEffect, useState } from "react";
 import styles from "./index.module.css";
 
 export default function Hamburger({ open }) {
+  const [y, setY] = useState(window.scrollY);
+  const hamburger = document.getElementById(styles.navicon3);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
+
+  const handleScroll = (e) => {
+    if (y > window.scrollY && window.scrollY < 40 && hamburger) {
+      // Scrolling upp
+      hamburger.style.right = "10px";
+      hamburger.style.transitionDuration = "300ms";
+    } else if (y < window.scrollY && window.scrollY > 20 && hamburger) {
+      // Scrolling down
+      hamburger.style.right = "40px";
+      hamburger.style.transitionDuration = "300ms";
+    }
+    setY(window.scrollY);
+  };
   return (
     <div className={open ? styles.open : ""} id={styles.navicon3}>
       <span></span>
