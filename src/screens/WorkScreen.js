bab212016df113b39728edfa105b0189274f54e1
sandrasmrekar <sandrasmrekar98@gmail.com>
2021-06-30 18:08:51 +0200

    added project item for this portfolio

--- src/screens/WorkScreen.js
+++ src/screens/WorkScreen.js
@@ -5,6 +5,8 @@ import styles from "./WorkScreen.module.css";
 export default function WorkScreen() {
   const text =
     "This is a e-commerce website that I did as a project to learn more about building with MERN stack, Mongo, E, React and Node.";
+  const portfolioText =
+    "Do you want to know how I build this portfolio websites? The source code is available on my github.";
   return (
     <div id={WORK_SECTION} className={styles.container}>
       <ProductItem>
@@ -31,6 +33,15 @@ export default function WorkScreen() {
         <ProductItem.Text
           text={text}
           link="https://github.com/sandrasmrekar/QuietZone"
+        />
+      </ProductItem>
+
+      <ProductItem>
+        <ProductItem.Header label="Portfolio" number="04" />
+        <ProductItem.SubHeader label="04 Portfolio" />
+        <ProductItem.Text
+          text={portfolioText}
+          link="https://github.com/sandrasmrekar/portfolio"
         />
       </ProductItem>
     </div>
