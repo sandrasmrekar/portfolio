255a58036ec60da70329a7fe1f110009b0faa36c
sandrasmrekar <sandrasmrekar98@gmail.com>
2021-06-30 18:34:16 +0200

    added text about the projects

--- src/screens/WorkScreen.js
+++ src/screens/WorkScreen.js
@@ -3,17 +3,22 @@ import { WORK_SECTION } from "../constants/sections";
 import styles from "./WorkScreen.module.css";
 // TODO: Visa projekt på ett snyggt sätt, kanske bildspel eller använda nummer sidomenu som finns på figma.
 export default function WorkScreen() {
-  const text =
+  const ecommerceText =
     "This is a e-commerce website that I did as a project to learn more about building with MERN stack, Mongo, E, React and Node.";
   const portfolioText =
     "Do you want to know how I build this portfolio websites? The source code is available on my github.";
+  const algorithmsText =
+    "This is a small project exploring sorting algorithms. Visit this project to see how it works.";
+  const quietZoneText =
+    "Quiet Zone is a mobile application created with React Native. This application puts the mobile on quiet mode, when the user is on a location where the user wishes not to be disturbed. ";
+
   return (
     <div id={WORK_SECTION} className={styles.container}>
       <ProductItem>
         <ProductItem.Header label="Green" number="01" />
         <ProductItem.SubHeader label="01 Green" />
         <ProductItem.Text
-          text={text}
+          text={ecommerceText}
           link="https://github.com/sandrasmrekar/green-ecommerce"
         />
       </ProductItem>
@@ -22,7 +27,7 @@ export default function WorkScreen() {
         <ProductItem.Header label="Algorithms" number="02" />
         <ProductItem.SubHeader label="02 Algorithms" />
         <ProductItem.Text
-          text={text}
+          text={algorithmsText}
           link="https://github.com/sandrasmrekar/sorting-algorithms"
         />
       </ProductItem>
@@ -31,7 +36,7 @@ export default function WorkScreen() {
         <ProductItem.Header label="Quiet Zone" number="03" />
         <ProductItem.SubHeader label="03 Quiet Zone" />
         <ProductItem.Text
-          text={text}
+          text={quietZoneText}
           link="https://github.com/sandrasmrekar/QuietZone"
         />
       </ProductItem>
