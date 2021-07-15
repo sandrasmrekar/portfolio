847272d0818964123087e2985db447598b28fb7d
sandrasmrekar <sandrasmrekar98@gmail.com>
2021-07-15 16:50:52 +0200

    ready for heroku

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "build")));
+app.get("/portfolio", function (req, res) {
+  return res.send("success");
+});
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+app.listen(port);
